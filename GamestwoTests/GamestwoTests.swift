09cdd8b83c756c3c28d01dcbb62546f1564a8f3b
Tsep1l0v <w1nd0wstsepilov@yandex.ru>
2024-09-26 14:47:16 +0300

    Initial Commit

---
+++ GamestwoTests/GamestwoTests.swift
@@ -0,0 +1,17 @@
+//
+//  GamestwoTests.swift
+//  GamestwoTests
+//
+//  Created by Дмитрий Цепилов on 26.09.2024.
+//
+
+import Testing
+@testable import Gamestwo
+
+struct GamestwoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
